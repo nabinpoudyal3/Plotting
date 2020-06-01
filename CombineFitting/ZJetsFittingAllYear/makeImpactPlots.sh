e8ecc8b947311978168b4d71338918b6be8be9d7
nabinpoudyal3 <fs2973@wayne.edu>
2020-06-01 12:03:11 -0500

    adding fit range for POI

--- CombineFitting/ZJetsFittingAllYear/makeImpactPlots.sh
+++ CombineFitting/ZJetsFittingAllYear/makeImpactPlots.sh
@@ -1,21 +1,21 @@
 text2workspace.py  datacard_CR123_2016.txt 
-combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125 -t -1  --expectSignal 1  --doInitialFit --robustFit 1 
-combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125 -t -1  --expectSignal 1 --robustFit 1 --doFits 
-combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125 -t -1  --expectSignal 1 -o impacts_toy_2016.json
+combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125 -t -1  --expectSignal 1  --doInitialFit --robustFit 1  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125 -t -1  --expectSignal 1 --robustFit 1 --doFits  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125 -t -1  --expectSignal 1 -o impacts_toy_2016.json  --rMin=0 --rMax=5
 plotImpacts.py -i impacts_toy_2016.json -o impacts_toy_2016
 
 
 text2workspace.py  datacard_CR123_2017.txt 
-combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125 -t -1  --expectSignal 1 --doInitialFit --robustFit 1 
-combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125 -t -1  --expectSignal 1 --robustFit 1 --doFits 
-combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125 -t -1  --expectSignal 1 -o impacts_toy_2017.json
+combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125 -t -1  --expectSignal 1 --doInitialFit --robustFit 1  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125 -t -1  --expectSignal 1 --robustFit 1 --doFits  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125 -t -1  --expectSignal 1 -o impacts_toy_2017.json  --rMin=0 --rMax=5
 plotImpacts.py -i impacts_toy_2017.json -o impacts_toy_2017
 
 
 text2workspace.py  datacard_CR123_2018.txt 
-combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125 -t -1  --expectSignal 1 --doInitialFit --robustFit 1 
-combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125 -t -1  --expectSignal 1 --robustFit 1 --doFits 
-combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125 -t -1  --expectSignal 1 -o impacts_toy_2018.json
+combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125 -t -1  --expectSignal 1 --doInitialFit --robustFit 1  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125 -t -1  --expectSignal 1 --robustFit 1 --doFits  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125 -t -1  --expectSignal 1 -o impacts_toy_2018.json  --rMin=0 --rMax=5
 plotImpacts.py -i impacts_toy_2018.json -o impacts_toy_2018
 
 
@@ -24,22 +24,22 @@ plotImpacts.py -i impacts_toy_2018.json -o impacts_toy_2018
 
 
 text2workspace.py  datacard_CR123_2016.txt 
-combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125  --doInitialFit --robustFit 1 
-combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125  --robustFit 1 --doFits 
-combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125  -o impacts_data_2016.json
+combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125  --doInitialFit --robustFit 1  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125  --robustFit 1 --doFits  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2016.root -m 125  -o impacts_data_2016.json  --rMin=0 --rMax=5
 plotImpacts.py -i impacts_data_2016.json -o impacts_data_2016
 
 
 
 text2workspace.py  datacard_CR123_2017.txt 
-combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125  --doInitialFit --robustFit 1 
-combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125  --robustFit 1 --doFits 
-combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125  -o impacts_data_2017.json
+combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125  --doInitialFit --robustFit 1  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125  --robustFit 1 --doFits  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2017.root -m 125  -o impacts_data_2017.json  --rMin=0 --rMax=5
 plotImpacts.py -i impacts_data_2017.json -o impacts_data_2017
 
 
 text2workspace.py  datacard_CR123_2018.txt 
-combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125   --doInitialFit --robustFit 1 
-combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125   --robustFit 1 --doFits 
-combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125   -o impacts_data_2018.json
+combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125   --doInitialFit --robustFit 1   --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125   --robustFit 1 --doFits  --rMin=0 --rMax=5
+combineTool.py -M Impacts -d datacard_CR123_2018.root -m 125   -o impacts_data_2018.json  --rMin=0 --rMax=5
 plotImpacts.py -i impacts_data_2018.json -o impacts_data_2018
