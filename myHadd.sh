6332dcde738603af6009ce7952ae3e501f324aac
nabinpoudyal3 <fs2973@wayne.edu>
2020-06-01 12:08:05 -0500

    add two histograms from same file but stored in different directory

---
+++ myHadd.sh
@@ -0,0 +1,325 @@
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_tightplots_2016
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_tightplots_2017
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_tightplots_2018
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRge2e0plots_2016
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRge2e0plots_2017
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRge2e0plots_2018
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe3ge2plots_2016
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe3ge2plots_2017
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe3ge2plots_2018
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRge4e0plots_2016
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRge4e0plots_2017
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRge4e0plots_2018
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe3e0plots_2016
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe3e0plots_2017
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe3e0plots_2018
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe2e1plots_2016
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe2e1plots_2017
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe2e1plots_2018
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe2e0plots_2016
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe2e0plots_2017
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe2e0plots_2018
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe2e2plots_2016
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe2e2plots_2017
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe2e2plots_2018
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe3e1plots_2016
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe3e1plots_2017
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+cd /uscms_data/d3/npoudyal/TTGammaSemiLeptonic13TeV/Plotting/ZJets_syst_looseCRe3e1plots_2018
+rm ele.root mu.root both.root
+rootmkdir ele.root:both
+rootmkdir mu.root:both
+rootcp ZJets_syst_Prefit.root:ele/data_obs     ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myBackground ele.root:both
+rootcp ZJets_syst_Prefit.root:ele/myZJets      ele.root:both
+rootcp ZJets_syst_Prefit.root:mu/data_obs     mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myBackground mu.root:both
+rootcp ZJets_syst_Prefit.root:mu/myZJets      mu.root:both
+hadd both.root ele.root mu.root
+
+echo "Done"
