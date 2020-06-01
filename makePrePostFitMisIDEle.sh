b377250642b2dbe0fda1bcbccb78d7a143bda5e3
nabinpoudyal3 <fs2973@wayne.edu>
2020-06-01 12:03:13 -0500

    .

--- makePrePostFitMisIDEle.sh
+++ makePrePostFitMisIDEle.sh
@@ -1,39 +1,14 @@
 #!/bin/bash
 
-
-#python makePlots_MisIDE.py -c Mu   -y 2016 --looseCRge2e0  --useQCDMC
-#python makePlots_MisIDE.py -c Ele  -y 2016 --looseCRge2e0  --useQCDMC
-
-#python makePlots_MisIDE.py -c Mu   -y 2017 --looseCRge2e0  --useQCDMC
-#python makePlots_MisIDE.py -c Ele  -y 2017 --looseCRge2e0  --useQCDMC
-
-#python makePlots_MisIDE.py -c Mu   -y 2018 --looseCRge2e0  --useQCDMC
-#python makePlots_MisIDE.py -c Ele  -y 2018 --looseCRge2e0  --useQCDMC
-
-#echo "Done prefit for MisIDEle plots!" 
-
-
-#wait
-
-#python makePlots_MisIDE.py -c Mu   -y 2016 --looseCRge2e0  --postfitPlots --useQCDMC
-#python makePlots_MisIDE.py -c Ele  -y 2016 --looseCRge2e0  --postfitPlots --useQCDMC
-
-#python makePlots_MisIDE.py -c Mu   -y 2017 --looseCRge2e0  --postfitPlots --useQCDMC
-#python makePlots_MisIDE.py -c Ele  -y 2017 --looseCRge2e0  --postfitPlots --useQCDMC
-
-#python makePlots_MisIDE.py -c Mu   -y 2018 --looseCRge2e0  --postfitPlots --useQCDMC
-#python makePlots_MisIDE.py -c Ele  -y 2018 --looseCRge2e0  --postfitPlots --useQCDMC
-
-#echo "Done postfit for MisIDEle plots!" 
-
-
 declare -a CONTROLREGION=("--looseCRge2e0" "--looseCRe2e0" "--looseCRe3e0" "--looseCRge4e0")
 declare -a YEAR=("2016" "2017" "2018")
 
 for controlregion in ${CONTROLREGION[@]}; do
 	for year in ${YEAR[@]}; do
-		python makePlots_MisIDE.py -y $year  -c Ele $controlregion  
-		python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  
+		echo "===> " python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --prefitPlots
+		python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --prefitPlots
+		echo "===> " python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --prefitPlots
+		python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --prefitPlots
 	done
 done
 wait
@@ -42,7 +17,9 @@ echo "Done prefit for MisIDEle plots!"
 
 for controlregion in ${CONTROLREGION[@]}; do
 	for year in ${YEAR[@]}; do
+		echo "===> " python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --postfitPlots
 		python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --postfitPlots
+		echo "===> " python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --postfitPlots
 		python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --postfitPlots
 	done
 done
