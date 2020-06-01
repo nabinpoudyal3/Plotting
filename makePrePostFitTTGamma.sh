2e7ee907f39bd63a858ae6783a589eebb7f5da9c
nabinpoudyal3 <fs2973@wayne.edu>
2020-06-01 12:03:13 -0500

    .

--- makePrePostFitTTGamma.sh
+++ makePrePostFitTTGamma.sh
@@ -4,20 +4,20 @@ declare -a YEAR=("2016")
 #declare -a YEAR=("2016" "2017" "2018")
 declare -a PLOT=("M3Plot" "ChIsoPlot" "btag0")
 
+#for plot in ${PLOT[@]}; do
+#	for year in ${YEAR[@]}; do
+		#python makePlots_TTGamma_temp.py -y $year   -c Ele   --$plot --prefitPlots
+		#python makePlots_TTGamma_temp.py -y $year   -c Mu    --$plot --prefitPlots
+#	done
+#done
+
+#wait
+
+
 for plot in ${PLOT[@]}; do
 	for year in ${YEAR[@]}; do
-		python makePlots_TTGamma.py -y $year   -c Ele   --$plot 
-		python makePlots_TTGamma.py -y $year   -c Mu    --$plot 
-	done
-done
-
-wait
-
-
-for plot in ${PLOT[@]}; do
-	for year in ${YEAR[@]}; do
-		python makePlots_TTGamma.py -y $year   -c Ele   --$plot --postfitPlots
-		python makePlots_TTGamma.py -y $year   -c Mu    --$plot --postfitPlots
+		python makePlots_TTGamma_temp.py -y $year   -c Ele   --$plot --postfitPlots
+		python makePlots_TTGamma_temp.py -y $year   -c Mu    --$plot --postfitPlots
 	done
 done
 
@@ -25,16 +25,16 @@ echo "Done making pre-post fit M3 and ChIso"
 
 
 echo "Making plot for 0 photon control region"
-for year in ${YEAR[@]}; do
+#for year in ${YEAR[@]}; do
 
-	python makePlot_M3Control.py -y $year --zeroPhoton  -c Ele 
-	python makePlot_M3Control.py -y $year --zeroPhoton  -c Mu   
+	#python makePlot_M3Control.py -y $year --zeroPhoton  -c Ele --prefitPlots
+	#python makePlot_M3Control.py -y $year --zeroPhoton  -c Mu  --prefitPlots
 
-done
+#done
 
-for year in ${YEAR[@]}; do
+#for year in ${YEAR[@]}; do
 
-	python makePlot_M3Control.py -y $year --zeroPhoton  -c Ele --postfitPlots
-	python makePlot_M3Control.py -y $year --zeroPhoton  -c Mu  --postfitPlots 
+	#python makePlot_M3Control.py -y $year --zeroPhoton  -c Ele --postfitPlots
+	#python makePlot_M3Control.py -y $year --zeroPhoton  -c Mu  --postfitPlots 
 
-done
+#done
