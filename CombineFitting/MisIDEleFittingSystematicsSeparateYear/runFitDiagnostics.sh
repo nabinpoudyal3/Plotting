a0538a39c936df15474f2147afd85be0dd29db6f
nabinpoudyal3 <fs2973@wayne.edu>
2020-06-01 12:03:11 -0500

    adding fit range for POI

--- CombineFitting/MisIDEleFittingSystematicsSeparateYear/runFitDiagnostics.sh
+++ CombineFitting/MisIDEleFittingSystematicsSeparateYear/runFitDiagnostics.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-combine -M FitDiagnostics -n CR123_2016   datacard_CR123_2016.txt -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
-combine -M FitDiagnostics -n CR1_2016     datacard_CR1_2016.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
-combine -M FitDiagnostics -n CR2_2016     datacard_CR2_2016.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
-combine -M FitDiagnostics -n CR3_2016     datacard_CR3_2016.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR123_2016   datacard_CR123_2016.txt -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR1_2016     datacard_CR1_2016.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR2_2016     datacard_CR2_2016.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR3_2016     datacard_CR3_2016.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
 
 
-combine -M FitDiagnostics -n CR123_2017   datacard_CR123_2017.txt -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  & 
-combine -M FitDiagnostics -n CR1_2017     datacard_CR1_2017.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
-combine -M FitDiagnostics -n CR2_2017     datacard_CR2_2017.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
-combine -M FitDiagnostics -n CR3_2017     datacard_CR3_2017.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR123_2017   datacard_CR123_2017.txt -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  & 
+combine -M FitDiagnostics -n CR1_2017     datacard_CR1_2017.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR2_2017     datacard_CR2_2017.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR3_2017     datacard_CR3_2017.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
 
-combine -M FitDiagnostics -n CR123_2018   datacard_CR123_2018.txt -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
-combine -M FitDiagnostics -n CR1_2018     datacard_CR1_2018.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
-combine -M FitDiagnostics -n CR2_2018     datacard_CR2_2018.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
-combine -M FitDiagnostics -n CR3_2018     datacard_CR3_2018.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF -v2 --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR123_2018   datacard_CR123_2018.txt -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR1_2018     datacard_CR1_2018.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR2_2018     datacard_CR2_2018.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
+combine -M FitDiagnostics -n CR3_2018     datacard_CR3_2018.txt   -s 314159  --redefineSignalPOIs r,ZGammaBkgPhotonSF,WGammaBkgPhotonSF,OtherSampleBkgPhotonSF -v2 --saveShape --robustFit=1 --saveNormalizations  &
 
 wait
 echo "Done Fitting"
