ad43c26fd891ee49c061978a3d5f9f05879c5aa0
nabinpoudyal3 <fs2973@wayne.edu>
2020-06-01 12:03:13 -0500

    .

--- makePrePostFitZJets.sh
+++ makePrePostFitZJets.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
 declare -a StringArray=("tight" "looseCRge2e0" "looseCRe3ge2" "looseCRge4e0" "looseCRe3e0" "looseCRe2e1" "looseCRe2e0" "looseCRe2e2" "looseCRe3e1" )
-for val in ${StringArray[@]}; do
-    python makePlots_ZJets.py -c DiMu   -y 2016 --useQCDMC --$val &
-    python makePlots_ZJets.py -c DiEle  -y 2016 --useQCDMC --$val &
-    python makePlots_ZJets.py -c DiMu   -y 2017 --useQCDMC --$val &
-    python makePlots_ZJets.py -c DiEle  -y 2017 --useQCDMC --$val &
-    python makePlots_ZJets.py -c DiMu   -y 2018 --useQCDMC --$val &
-    python makePlots_ZJets.py -c DiEle  -y 2018 --useQCDMC --$val &
-   
-done
-wait
-echo "Done Prefit!" 
+#for val in ${StringArray[@]}; do
+#	echo "===>" python makePlots_ZJets.py -c DiMu   -y 2016 --useQCDMC --prefitPlots --$val
+#    python makePlots_ZJets.py -c DiMu   -y 2016 --useQCDMC --prefitPlots --$val 
+#	echo "===>" python makePlots_ZJets.py -c DiEle  -y 2016 --useQCDMC --prefitPlots --$val
+#    python makePlots_ZJets.py -c DiEle  -y 2016 --useQCDMC --prefitPlots --$val 
+#	echo "===>" python makePlots_ZJets.py -c DiMu   -y 2017 --useQCDMC --prefitPlots --$val
+#    python makePlots_ZJets.py -c DiMu   -y 2017 --useQCDMC --prefitPlots --$val 
+#	echo "===>" python makePlots_ZJets.py -c DiEle  -y 2017 --useQCDMC --prefitPlots --$val
+#    python makePlots_ZJets.py -c DiEle  -y 2017 --useQCDMC --prefitPlots --$val 
+#	echo "===>" python makePlots_ZJets.py -c DiMu   -y 2018 --useQCDMC --prefitPlots --$val
+#    python makePlots_ZJets.py -c DiMu   -y 2018 --useQCDMC --prefitPlots --$val 
+#	echo "===>" python makePlots_ZJets.py -c DiEle  -y 2018 --useQCDMC --prefitPlots --$val
+#    python makePlots_ZJets.py -c DiEle  -y 2018 --useQCDMC --prefitPlots --$val 
+#   
+#done
+#wait
+#echo "Done Prefit!" 
 
 
 for val in ${StringArray[@]}; do
-    python makePlots_ZJets.py -c DiMu   -y 2016 --useQCDMC --postfitPlots --$val &
-    python makePlots_ZJets.py -c DiEle  -y 2016 --useQCDMC --postfitPlots --$val &
-    python makePlots_ZJets.py -c DiMu   -y 2017 --useQCDMC --postfitPlots --$val &
-    python makePlots_ZJets.py -c DiEle  -y 2017 --useQCDMC --postfitPlots --$val &
-    python makePlots_ZJets.py -c DiMu   -y 2018 --useQCDMC --postfitPlots --$val &
-    python makePlots_ZJets.py -c DiEle  -y 2018 --useQCDMC --postfitPlots --$val &
+	echo "===>" python makePlots_ZJets.py -c DiMu   -y 2016 --useQCDMC --postfitPlots --$val 
+    python makePlots_ZJets.py -c DiMu   -y 2016 --useQCDMC --postfitPlots --$val 
+	echo "===>" python makePlots_ZJets.py -c DiEle  -y 2016 --useQCDMC --postfitPlots --$val 
+    python makePlots_ZJets.py -c DiEle  -y 2016 --useQCDMC --postfitPlots --$val 
+	echo "===>" python makePlots_ZJets.py -c DiMu   -y 2017 --useQCDMC --postfitPlots --$val 
+    python makePlots_ZJets.py -c DiMu   -y 2017 --useQCDMC --postfitPlots --$val 
+	echo "===>" python makePlots_ZJets.py -c DiEle  -y 2017 --useQCDMC --postfitPlots --$val 
+    python makePlots_ZJets.py -c DiEle  -y 2017 --useQCDMC --postfitPlots --$val 
+	echo "===>" python makePlots_ZJets.py -c DiMu   -y 2018 --useQCDMC --postfitPlots --$val 
+    python makePlots_ZJets.py -c DiMu   -y 2018 --useQCDMC --postfitPlots --$val 
+	echo "===>" python makePlots_ZJets.py -c DiEle  -y 2018 --useQCDMC --postfitPlots --$val 
+    python makePlots_ZJets.py -c DiEle  -y 2018 --useQCDMC --postfitPlots --$val 
     
 done
 wait 
