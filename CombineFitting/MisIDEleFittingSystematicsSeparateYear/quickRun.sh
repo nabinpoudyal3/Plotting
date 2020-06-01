1408090a48aadd7f1287b260c732387ad98b10a3
nabinpoudyal3 <fs2973@wayne.edu>
2020-06-01 12:07:19 -0500

    separete systematic and statistical uncertainties

---
+++ CombineFitting/MisIDEleFittingSystematicsSeparateYear/quickRun.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+##$1= control region with year
+declare -a CONTROLREGION=("CR123" "CR1" "CR2" "CR3")
+declare -a YEAR=("2016" "2017" "2018")
+#declare -a YEAR=("2018")
+rMin=0
+rMax=5
+for year in ${YEAR[@]}; do
+	for controlregion in ${CONTROLREGION[@]}; do
+		text2workspace.py datacard_"$controlregion"_"$year".txt
+		combine -M MultiDimFit datacard_"$controlregion"_"$year".root  -s 314159  --rMin $rMin --rMax $rMax --algo grid --points 300 -n .Main
+#python plot1DScan.py higgsCombine.Main.MultiDimFit.mH120.314159.root -o single_scan
+		combine -M MultiDimFit datacard_"$controlregion"_"$year".root -n .snapshot -s 314159  --rMin $rMin --rMax $rMax --saveWorkspace
+		combine -M MultiDimFit higgsCombine.snapshot.MultiDimFit.mH120.314159.root -s 314159  -n .freezeAll --rMin $rMin --rMax $rMax --algo grid --points 300 --freezeNuisanceGroups mySyst --snapshotName MultiDimFit
+		python plot1DScan.py  higgsCombine.Main.MultiDimFit.mH120.314159.root --others 'higgsCombine.freezeAll.MultiDimFit.mH120.314159.root:FreezeAll:2' --breakdown Syst,Stat -o freeze_"$controlregion"_"$year"
+##
+#freezing nuisance parameter gives better result
+	done
+done
