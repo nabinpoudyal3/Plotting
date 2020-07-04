46bded4eb0ea5edbd39a4885de0029edf0e1b5bf
nabinpoudyal3 <fs2973@wayne.edu>
2020-07-04 18:45:01 -0500

    .

--- Condor_Histogramming/directoryCreatorSys.py
+++ Condor_Histogramming/directoryCreatorSys.py
@@ -1,4 +1,4 @@
-#!/bin/bash
+import sys
 
 #ControlRegion=["tight", "looseCRge2e0", "looseCRge2ge0", "looseCRe3ge2", "looseCRge4e0", "looseCRe3e0", "looseCRe2e1", "looseCRe2e0", "looseCRe2e2", "looseCRe3e1" ]
 #systematics=["isr","fsr"] 
@@ -21,8 +21,30 @@
 #with open("systematicsDirListDilepton.sh","w") as _file:
 #    _file.write(lineSyst) 
     
+    
 ControlRegion=["tight", "looseCRge2e0", "looseCRge2ge0", "looseCRe3ge2", "looseCRge4e0", "looseCRe3e0", "looseCRe2e1", "looseCRe2e0", "looseCRe2e2", "looseCRe3e1" ]
-systematics=["isr","fsr","Pdf"] 
+#systematics=["isr","fsr","Pdf"] 
+systematics=["prefireEcal"] 
+lineSyst ='#!/bin/bash \n'
+lineSyst ="eosmkdir='eos root://cmseos.fnal.gov mkdir' \n"
+for channel in ['ele', 'mu']:
+	for year in ["2016", "2017", "2018"]:
+		for cr in ControlRegion:
+			for syst in systematics:
+				lineSyst+='echo eosmkdir -p /store/user/npoudyal/histograms_%s/%s/Dilep_hists_%s_up_%s/ \n'%(year,channel,syst,cr) 
+				lineSyst+='$eosmkdir -p /store/user/npoudyal/histograms_%s/%s/Dilep_hists_%s_up_%s/ \n'%(year,channel,syst,cr) 
+				lineSyst+='echo eosmkdir -p /store/user/npoudyal/histograms_%s/%s/Dilep_hists_%s_down_%s/ \n'%(year,channel,syst,cr) 
+				lineSyst+='$eosmkdir -p /store/user/npoudyal/histograms_%s/%s/Dilep_hists_%s_down_%s/ \n'%(year,channel,syst,cr) 
+
+with open("makeDirDilep_prefireEcal.sh","w") as _file:
+    _file.write(lineSyst)  
+    
+sys.exit()
+
+
+ControlRegion=["tight", "looseCRge2e0", "looseCRge2ge0", "looseCRe3ge2", "looseCRge4e0", "looseCRe3e0", "looseCRe2e1", "looseCRe2e0", "looseCRe2e2", "looseCRe3e1" ]
+#systematics=["isr","fsr","Pdf"] 
+systematics=["prefireEcal"] 
 lineSyst ='#!/bin/bash \n'
 lineSyst +="eosls='eos root://cmseos.fnal.gov ls -lh' \n"
 for channel in ['ele', 'mu']:
@@ -31,8 +53,8 @@ for channel in ['ele', 'mu']:
 			for syst in systematics:
 				lineSyst+='echo $eosls /store/user/npoudyal/histograms_%s/%s/Dilep_hists_%s_up_%s/ \n'%(year,channel,syst,cr) 
 				lineSyst+='$eosls /store/user/npoudyal/histograms_%s/%s/Dilep_hists_%s_up_%s/ \n'%(year,channel,syst,cr) 
-				lineSyst+='echo $eosls /store/user/npoudyal/histograms_%s/%s/Dilep_hists_%s_up_%s/ \n'%(year,channel,syst,cr) 
+				lineSyst+='echo $eosls /store/user/npoudyal/histograms_%s/%s/Dilep_hists_%s_down_%s/ \n'%(year,channel,syst,cr) 
 				lineSyst+='$eosls /store/user/npoudyal/histograms_%s/%s/Dilep_hists_%s_down_%s/ \n'%(year,channel,syst,cr) 
 
-with open("ListFilesDilepton.sh","w") as _file:
+with open("ListFilesDilepton_prefireEcal.sh","w") as _file:
     _file.write(lineSyst)
