499f5d1481f380b2622ca8552f2efe1e50fc963a
nabinpoudyal3 <fs2973@wayne.edu>
2020-07-04 18:45:03 -0500

    added Ecal prefire scale factor

--- makePrePostFitTTGamma.sh
+++ makePrePostFitTTGamma.sh
@@ -4,37 +4,44 @@ declare -a YEAR=("2016")
 #declare -a YEAR=("2016" "2017" "2018")
 declare -a PLOT=("M3Plot" "ChIsoPlot" "btag0")
 
-#for plot in ${PLOT[@]}; do
-#	for year in ${YEAR[@]}; do
-		#python makePlots_TTGamma_temp.py -y $year   -c Ele   --$plot --prefitPlots
-		#python makePlots_TTGamma_temp.py -y $year   -c Mu    --$plot --prefitPlots
-#	done
-#done
-
-#wait
-
-
 for plot in ${PLOT[@]}; do
 	for year in ${YEAR[@]}; do
-		python makePlots_TTGamma_temp.py -y $year   -c Ele   --$plot --postfitPlots
-		python makePlots_TTGamma_temp.py -y $year   -c Mu    --$plot --postfitPlots
+		echo python makePlots_TTGamma.py -y $year   -c Ele   --$plot --prefitPlots 
+		python makePlots_TTGamma.py -y $year   -c Ele   --$plot --prefitPlots 
+		echo python makePlots_TTGamma.py -y $year   -c Mu    --$plot --prefitPlots 
+		python makePlots_TTGamma.py -y $year   -c Mu    --$plot --prefitPlots 
+	done
+done
+#
+wait
+#
+#
+for plot in ${PLOT[@]}; do
+	for year in ${YEAR[@]}; do
+		echo python makePlots_TTGamma.py -y $year   -c Ele   --$plot --postfitPlots 
+		python makePlots_TTGamma.py -y $year   -c Ele   --$plot --postfitPlots 
+		echo python makePlots_TTGamma.py -y $year   -c Mu    --$plot --postfitPlots 
+		python makePlots_TTGamma.py -y $year   -c Mu    --$plot --postfitPlots 
 	done
 done
 
 echo "Done making pre-post fit M3 and ChIso"
 
-
 echo "Making plot for 0 photon control region"
-#for year in ${YEAR[@]}; do
+for year in ${YEAR[@]}; do
 
-	#python makePlot_M3Control.py -y $year --zeroPhoton  -c Ele --prefitPlots
-	#python makePlot_M3Control.py -y $year --zeroPhoton  -c Mu  --prefitPlots
+	echo python makePlot_M3Control.py -y $year --zeroPhoton  -c Ele --prefitPlots 
+	python makePlot_M3Control.py -y $year --zeroPhoton  -c Ele --prefitPlots 
+	echo python makePlot_M3Control.py -y $year --zeroPhoton  -c Mu  --prefitPlots 
+	python makePlot_M3Control.py -y $year --zeroPhoton  -c Mu  --prefitPlots 
+ 
+done
 
-#done
+for year in ${YEAR[@]}; do
 
-#for year in ${YEAR[@]}; do
+	echo python makePlot_M3Control.py -y $year --zeroPhoton  -c Ele --postfitPlots 
+	python makePlot_M3Control.py -y $year --zeroPhoton  -c Ele --postfitPlots 
+	echo python makePlot_M3Control.py -y $year --zeroPhoton  -c Mu  --postfitPlots 
+	python makePlot_M3Control.py -y $year --zeroPhoton  -c Mu  --postfitPlots 
 
-	#python makePlot_M3Control.py -y $year --zeroPhoton  -c Ele --postfitPlots
-	#python makePlot_M3Control.py -y $year --zeroPhoton  -c Mu  --postfitPlots 
-
-#done
+done
