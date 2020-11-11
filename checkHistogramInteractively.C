fe2fd4d02903cd622bd010b9bfcb904a6582b108
nabinpoudyal3 <fs2973@wayne.edu>
2020-11-11 17:20:09 -0600

    quick backup

---
+++ checkHistogramInteractively.C
@@ -0,0 +1,17 @@
+{
+TChain chain("AnalysisTree");
+chain.Add("root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM10to50_2016_AnalysisNtuple.root");
+chain.Add("root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM50_2016_AnalysisNtuple_1of5.root");
+chain.Add("root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM50_2016_AnalysisNtuple_2of5.root");
+chain.Add("root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM50_2016_AnalysisNtuple_3of5.root");
+chain.Add("root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM50_2016_AnalysisNtuple_4of5.root");
+chain.Add("root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM50_2016_AnalysisNtuple_5of5.root");
+
+AnalysisTree->Draw("nJet","(passPresel_Ele && nJet>=2 && nBJet==0 && nPho==1 )*evtWeight*prefireSF*PUweight*muEffWeight*eleEffWeight*1.0*1.0*1.0*1.0*btagWeight_1a","hist");
+}
+
+
+
+// for 2016: AnalysisTree->Draw("phoEta:phoPhi","(passPresel_Ele && nJet==2 && nBJet==0 && nPho==1 && !(((phoPhi>2.3 && phoPhi<2.7) && phoEta<0.1) || ((phoPhi>1.2 && phoPhi<1.6) && (phoEta>-1 && phoEta<0.8)) || ((phoPhi>-1.7 && phoPhi<-1.4) && (phoEta>-1 && phoEta<0.9))) )*evtWeight*PUweight*muEffWeight*eleEffWeight*1.0*1.0*btagWeight_1a","colz");
+// for 2016: AnalysisTree->Draw("phoEta:phoPhi","(passPresel_Ele && nJet==2 && nBJet==0 && nPho==1 && !(((phoPhi>2.7 && phoPhi<3.1) && phoEta>1.2)))*evtWeight*PUweight*muEffWeight*eleEffWeight*1.0*1.0*btagWeight_1a","colz");
+// for 2018: AnalysisTree->Draw("phoEta:phoPhi","(passPresel_Ele && nJet==2 && nBJet==0 && nPho==1 && !(((phoPhi>0.4 && phoPhi<0.8) && phoEta>0)))*evtWeight*PUweight*muEffWeight*eleEffWeight*1.0*1.0*btagWeight_1a","colz");
