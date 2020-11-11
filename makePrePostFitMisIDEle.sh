83fc60fa9d6fc018c3d5885495f6e01d76ca8c33
nabinpoudyal3 <fs2973@wayne.edu>
2020-11-11 17:22:11 -0600

    quick backup

--- makePrePostFitMisIDEle.sh
+++ makePrePostFitMisIDEle.sh
@@ -6,9 +6,9 @@ declare -a YEAR=("2016" "2017" "2018")
 for controlregion in ${CONTROLREGION[@]}; do
 	for year in ${YEAR[@]}; do
 		echo "===> " python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --prefitPlots
-		python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --prefitPlots
+		             python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --prefitPlots
 		echo "===> " python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --prefitPlots
-		python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --prefitPlots
+		             python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --prefitPlots
 	done
 done
 wait
@@ -18,9 +18,9 @@ echo "Done prefit for MisIDEle plots!"
 for controlregion in ${CONTROLREGION[@]}; do
 	for year in ${YEAR[@]}; do
 		echo "===> " python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --postfitPlots
-		python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --postfitPlots
+		             python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --postfitPlots
 		echo "===> " python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --postfitPlots
-		python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --postfitPlots
+		             python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --postfitPlots
 	done
 done
 wait
