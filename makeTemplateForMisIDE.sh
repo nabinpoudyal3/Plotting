0d23a9109245daf04353e87d7474cfba218a85bb
nabinpoudyal3 <fs2973@wayne.edu>
2020-11-11 17:22:11 -0600

    quick backup

--- makeTemplateForMisIDE.sh
+++ makeTemplateForMisIDE.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 
+###IMPORTANT:
+# isr, fsr and q2 are wrong.
+
 declare -a CONTROLREGION=("--looseCRge2e0" "--looseCRe2e0" "--looseCRe3e0" "--looseCRge4e0")
 declare -a YEAR=("2016" "2017" "2018")
-declare -a SYSTEMATICS=("BTagSF_b" "BTagSF_l" "PU" "MuEff" "EleEff" "PhoEff" "Q2" "isr" "fsr" "Pdf" "prefireEcal")
+declare -a SYSTEMATICS=("BTagSF_b" "BTagSF_l" "PU" "MuEff" "EleEff" "PhoEff" "prefireEcal" "JER" "JECTotal")
 declare -a LEVEL=("up" "down")
+
 
 
 for controlregion in ${CONTROLREGION[@]}; do
 	for year in ${YEAR[@]}; do
 		echo "===>  python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --template"
-		python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --template 
+		            python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --template 
 		echo "===>  python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --template"
-		python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --template
+		            python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --template
 	done
 done
+
+
 
 for controlregion in ${CONTROLREGION[@]}; do
 	for year in ${YEAR[@]}; do
 		for systematics in ${SYSTEMATICS[@]}; do
 			for level in ${LEVEL[@]}; do
 				echo "===>  python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --syst $systematics --level $level --template"
-				python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --syst $systematics --level $level --template
+				            python makePlots_MisIDE.py -y $year  -c Ele $controlregion  --syst $systematics --level $level --template
 				echo "===>  python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --syst $systematics --level $level --template"
-				python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --syst $systematics --level $level --template
+				            python makePlots_MisIDE.py -y $year  -c Mu  $controlregion  --syst $systematics --level $level --template
 				
 			done
 		done
 	done
 done
 echo "Done making templates with QCD_DD for MisIDE."
+
