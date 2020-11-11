d7760990e6e5fe203ac1bf23c86d138a44376de3
nabinpoudyal3 <fs2973@wayne.edu>
2020-11-11 17:22:09 -0600

    quick backup

--- Condor_Histogramming/setup_condor_Histogramming.sh
+++ Condor_Histogramming/setup_condor_Histogramming.sh
@@ -2,9 +2,9 @@
 rm makeHistograms.py
 rm sampleInformation.py
 rm HistogramListDict.py
-
+rm myListOfHistograms.py
 cd ..
-tar cfP myHistograms.tar makeHistograms.py sampleInformation.py HistogramListDict.py
+tar cfP myHistograms.tar makeHistograms.py sampleInformation.py HistogramListDict.py myListOfHistograms.py
 mv myHistograms.tar Condor_Histogramming
 cd Condor_Histogramming
 xrdcp -f myHistograms.tar  root://cmseos.fnal.gov//store/user/npoudyal/
