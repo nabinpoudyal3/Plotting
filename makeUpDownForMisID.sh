d9cfb54a863c0d6f46996a45e92d105fd3c07713
nabinpoudyal3 <fs2973@wayne.edu>
2020-11-11 17:22:11 -0600

    quick backup

--- makeUpDownForMisID.sh
+++ makeUpDownForMisID.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+declare -a CONTROLREGION=("looseCRge2e0" "looseCRge4e0" "looseCRe3e0" "looseCRe2e0" )
 declare -a YEAR=("2016" "2017" "2018")
 
+#declare -a CONTROLREGION=("looseCRge2e0")
+#declare -a YEAR=("2016")
 
 for year in ${YEAR[@]}; do
-	# rm -rf M3ChIso_tightplots_"$yr"/*.pdf # delete old pdf files before merging new ones
-	python makePlots_SystUpDownNominal.py -y $year -c Ele  
-	python makePlots_SystUpDownNominal.py -y $year -c Ele  
-	
-	python makePlots_SystUpDownNominal.py -y $year -c Mu    
-	python makePlots_SystUpDownNominal.py -y $year -c Mu  
+        # rm -rf M3ChIso_tightplots_"$yr"/*.pdf # delete old pdf files before merging new ones
+        for controlregion in ${CONTROLREGION[@]}; do
+                echo "===> python makePlots_SystUpDownMisID.py -y $year --$controlregion" 
+                python makePlots_SystUpDownMisID.py -y $year  --$controlregion
+
+        done
 done
 
