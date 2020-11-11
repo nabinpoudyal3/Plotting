f2f0031df8b30ec5a9d80c3a7b8ce9351cb2411d
nabinpoudyal3 <fs2973@wayne.edu>
2020-11-11 17:22:09 -0600

    quick backup

--- Condor_Histogramming/runCondorAtOnce.sh
+++ Condor_Histogramming/runCondorAtOnce.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+rm condor/*.*
+
+condor_submit condor_makeHistograms_JECTotal.jdl
+
+
+
 condor_submit condor_makeHistograms_nominal.jdl
 condor_submit condor_makeHistograms_PU.jdl
 condor_submit condor_makeHistograms_MuEff.jdl
@@ -8,8 +14,8 @@ condor_submit condor_makeHistograms_BTagSF_b.jdl
 condor_submit condor_makeHistograms_BTagSF_l.jdl
 condor_submit condor_makeHistograms_EleEff.jdl
 condor_submit condor_makeHistograms_Q2.jdl
-#condor_submit condor_makeHistograms_Pdf.jdl
-#condor_submit condor_makeHistograms_isr.jdl
-#condor_submit condor_makeHistograms_fsr.jdl
-
-
+condor_submit condor_makeHistograms_Pdf.jdl
+condor_submit condor_makeHistograms_isr.jdl
+condor_submit condor_makeHistograms_fsr.jdl
+condor_submit condor_makeHistograms_prefireEcal.jdl
+condor_submit condor_makeHistograms_JER.jdl
