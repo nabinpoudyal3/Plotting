fa2f33b9fb68eba3245d32d00d59f847f8dfbff7
nabinpoudyal3 <fs2973@wayne.edu>
2020-11-11 17:22:08 -0600

    quick backup

--- Condor_Histogramming/checkFiles.sh
+++ Condor_Histogramming/checkFiles.sh
@@ -3,11 +3,11 @@ eosls="eos root://cmseos.fnal.gov ls"
 for dir in `$eosls /store/user/npoudyal/histograms_*`;
 do
 #echo $dir
-	for dir1 in `$eosls /store/user/npoudyal/$dir`;do
+	for dir1 in `$eosls /store/user/npoudyal/TEST/$dir`;do
 		#echo $dir/$dir1
-		for dir2 in `$eosls /store/user/npoudyal/$dir/$dir1`;do
+		for dir2 in `$eosls /store/user/npoudyal/TEST/$dir/$dir1`;do
 		echo $dir/$dir1/$dir2
-		$eosls -lth /store/user/npoudyal/$dir/$dir1/$dir2
+		$eosls -lth /store/user/npoudyal/TEST/$dir/$dir1/$dir2
 		done
 	done
 done
