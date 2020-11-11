e605fff18ca9d3100224238556f9edd08180fa7f
nabinpoudyal3 <fs2973@wayne.edu>
2020-11-11 17:22:12 -0600

    quick backup

--- makeUpDownForTTGamma.sh
+++ makeUpDownForTTGamma.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#declare -a YEAR=("2016" "2017" "2018")
+declare -a YEAR=("2016" "2017" "2018")
 
-#for year in ${YEAR[@]}; do
-	# rm -rf M3ChIso_tightplots_"$yr"/*.pdf # delete old pdf files before merging new ones
-#	python makePlots_SystUpDownNominal_ttg.py -y $year -c Ele --tight --M3Plot 
-#	python makePlots_SystUpDownNominal_ttg.py -y $year -c Ele --tight --ChIsoPlot
-	
-#	python makePlots_SystUpDownNominal_ttg.py -y $year -c Mu  --tight --M3Plot 
-#	python makePlots_SystUpDownNominal_ttg.py -y $year -c Mu  --tight --ChIsoPlot
-#done
+for year in ${YEAR[@]}; do
+
+python makePlots_SystUpDownTTGamma.py -y $year -c Ele --tight --plotVariable M3 
+python makePlots_SystUpDownTTGamma.py -y $year -c Ele --tight --plotVariable ChIso 
+python makePlots_SystUpDownTTGamma.py -y $year -c Ele --tight --plotVariable zerobtag
+python makePlots_SystUpDownTTGamma.py -y $year -c Ele --tight --plotVariable M30photon
 
 
-python makePlots_SystUpDownNominal_ttg.py -y 2016 -c Ele --tight --plotVariable M3 
-python makePlots_SystUpDownNominal_ttg.py -y 2016 -c Ele --tight --plotVariable ChIso 
-python makePlots_SystUpDownNominal_ttg.py -y 2016 -c Ele --tight --plotVariable zerobtag
-python makePlots_SystUpDownNominal_ttg.py -y 2016 -c Ele --tight --plotVariable M30photon
+python makePlots_SystUpDownTTGamma.py -y $year -c Mu --tight --plotVariable M3 
+python makePlots_SystUpDownTTGamma.py -y $year -c Mu --tight --plotVariable ChIso 
+python makePlots_SystUpDownTTGamma.py -y $year -c Mu --tight --plotVariable zerobtag
+python makePlots_SystUpDownTTGamma.py -y $year -c Mu --tight --plotVariable M30photon
+
+done 
