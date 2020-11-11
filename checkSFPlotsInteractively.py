57d23179dbf6ce5507b1793420c0de7bfb4e1a21
nabinpoudyal3 <fs2973@wayne.edu>
2020-11-11 17:20:09 -0600

    quick backup

---
+++ checkSFPlotsInteractively.py
@@ -0,0 +1,114 @@
+import ROOT
+
+ZJets = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM10to50_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM50_2016_AnalysisNtuple_1of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM50_2016_AnalysisNtuple_2of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM50_2016_AnalysisNtuple_3of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM50_2016_AnalysisNtuple_4of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/DYjetsM50_2016_AnalysisNtuple_5of5.root",]
+
+GJets= [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/GJets_HT100To200_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/GJets_HT200To400_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/GJets_HT400To600_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/GJets_HT40To100_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/GJets_HT600ToInf_2016_AnalysisNtuple.root",]
+
+QCDEle = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt20to30_Ele_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt30to50_Ele_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt50to80_Ele_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt80to120_Ele_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt120to170_Ele_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt170to300_Ele_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt300toInf_Ele_2016_AnalysisNtuple.root",]
+
+QCDMu = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt20to30_Mu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt30to50_Mu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt50to80_Mu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt80to120_Mu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt120to170_Mu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt170to300_Mu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt300to470_Mu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt470to600_Mu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt600to800_Mu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt800to1000_Mu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/QCD_Pt1000toInf_Mu_2016_AnalysisNtuple.root",]
+
+SingleTop = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/ST_s_channel_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/ST_tW_channel_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/ST_t_channel_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/ST_tbarW_channel_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/ST_tbar_channel_2016_AnalysisNtuple.root",]
+
+TopGamma = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TGJets_2016_AnalysisNtuple.root",]
+
+TTGamma = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTGamma_Dilepton_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTGamma_Hadronic_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTGamma_SingleLept_2016_AnalysisNtuple.root",]
+
+TTV = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTWtoLNu_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTWtoQQ_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTZtoLL_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTZtoLL_M1to10_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTZtoQQ_2016_AnalysisNtuple.root",]
+
+TTbar = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Dilepton_2016_AnalysisNtuple_1of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Dilepton_2016_AnalysisNtuple_2of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Dilepton_2016_AnalysisNtuple_3of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Dilepton_2016_AnalysisNtuple_4of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Dilepton_2016_AnalysisNtuple_5of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Hadronic_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Semilept_2016_AnalysisNtuple_1of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Semilept_2016_AnalysisNtuple_2of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Semilept_2016_AnalysisNtuple_3of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Semilept_2016_AnalysisNtuple_4of5.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/TTbarPowheg_Semilept_2016_AnalysisNtuple_5of5.root",]
+
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/VVTo2L2Nu_2016_AnalysisNtuple.root",
+
+WJets = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/W1jets_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/W2jets_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/W3jets_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/W4jets_2016_AnalysisNtuple.root",]
+
+WGamma = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/WGamma_01J_5f_2016_AnalysisNtuple.root",]
+
+ZGamma = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/ZGamma_01J_5f_lowMass_2016_AnalysisNtuple.root",]
+
+Diboson = [
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/WW_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/WZ_2016_AnalysisNtuple.root",
+"root://cmseos.fnal.gov//store/user/lpctop/TTGamma_FullRun2/AnalysisNtuples/2016/ZZ_2016_AnalysisNtuple.root",]
+
+#ZJets + GJets + QCDEle + QCDMu + SingleTop + TopGamma + TTGamma + TTV + TTbar + WJets + WGamma + ZGamma + Diboson
+
+ROOT.gROOT.SetBatch(True)
+tree = ROOT.TChain("AnalysisTree")
+
+for item in ZGamma: #ZJets + GJets + QCDEle + QCDMu + SingleTop + TopGamma + TTGamma + TTV + TTbar + WJets + WGamma + ZGamma + Diboson:
+	tree.Add(item)
+
+weights = ["PUweight","btagWeight_1a","btagSF","muEffWeight","prefireSF","eleEffWeight","phoEffWeight","loosePhoEffWeight","evtWeight"] #,"q2weight_nominal","pdfWeight",""]
+
+for  weight in weights:
+	#histo = ROOT.TH1F("histo","",90,-1,2)
+	canvas = ROOT.TCanvas("canvas","",800,800)
+	#histo = ROOT.TH1F()
+	#if weight == "evtWeight": tree.Draw("%s"%weight,"","hist") 
+	#else:	tree.Draw("%s>>histo"%weight,"","hist")  
+	tree.Draw("%s"%weight,"","hist") 
+	canvas.Print("WeightPlots/%s.pdf"%(weight))
+	#del histo;
+	del canvas;
+
