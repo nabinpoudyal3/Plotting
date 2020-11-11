72907547144f4cc7b6b1a39c204108b3b575ffde
nabinpoudyal3 <fs2973@wayne.edu>
2020-11-11 17:22:09 -0600

    quick backup

--- Condor_Histogramming/runCondorAtOnceDilepton.sh
+++ Condor_Histogramming/runCondorAtOnceDilepton.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+rm condor/*.*
+
+condor_submit condor_makeHistograms_Dilep_JECTotal.jdl
+condor_submit condor_makeHistograms_Dilep_JER.jdl
+
+exit 1
 condor_submit condor_makeHistograms_Dilep_nominal.jdl
 condor_submit condor_makeHistograms_Dilep_PU.jdl
 condor_submit condor_makeHistograms_Dilep_MuEff.jdl
@@ -8,8 +14,8 @@ condor_submit condor_makeHistograms_Dilep_BTagSF_b.jdl
 condor_submit condor_makeHistograms_Dilep_BTagSF_l.jdl
 condor_submit condor_makeHistograms_Dilep_EleEff.jdl
 condor_submit condor_makeHistograms_Dilep_Q2.jdl
-#condor_submit condor_makeHistograms_Dilep_Pdf.jdl
-#condor_submit condor_makeHistograms_Dilep_isr.jdl
-#condor_submit condor_makeHistograms_Dilep_fsr.jdl
-
+condor_submit condor_makeHistograms_Dilep_Pdf.jdl
+condor_submit condor_makeHistograms_Dilep_isr.jdl
+condor_submit condor_makeHistograms_Dilep_fsr.jdl
+condor_submit condor_makeHistograms_Dilep_prefireEcal.jdl
 
